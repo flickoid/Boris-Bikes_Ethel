1ca74f04067540dc06ef02ede500dd025d93ad40
flickoid <chris.oatley@gmail.com>
2014-08-18 12:10:56 +0100

    Addedinitialize and break! methods

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,8 +1,15 @@
 require 'bike'
 
-describe Bike do 
+describe Bike do
+
+let (:bike) {Bike.new} 
 	it 'should not be broken after we create it' do 
-		bike = Bike.new 
 		expect(bike).not_to be_broken
 	end
+
+	it "should be able to break" do
+		bike.break!
+		expect(bike).to be_broken
+	end
+
 end
